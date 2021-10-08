823b2ec2d7eb549c309636642401050e7538bbe4
pavan <pavan_fw10_057@masai.school>
2021-10-09 00:20:25 +0530

    firstcommit

---
+++ react-portfolio/frontend/src/projectData.js
@@ -0,0 +1,23 @@
+const projects = [
+  
+  {
+    title: 'Udemy',
+    image: './images/udemy',
+    description: 'Udemy is a web application for learning online. udemy is intended to be a source of education where the instructors are self-motivated and passionate for the courses they create',
+    tools: ['HTML', 'CSS', 'JAVA SCRIPT'],
+    github:  'https://github.com/sachanarpit/udemy_clone',
+    link: 'https://sachanarpit.github.io/udemy_clone/',
+
+},
+  {
+    title: 'Makemytrip',
+    image: './images/landingpagesst',
+    description: 'Makemytrip  provides online travel services including flight tickets, domestic and international holiday packages, hotel reservations.',
+    tools: ['Javascript', 'REACT','NODE js','MongoDB'],
+    github: 'https://github.com/nitish14920/MakeMyTrip',
+    // link: '',
+  },
+
+];
+
+export default projects;
