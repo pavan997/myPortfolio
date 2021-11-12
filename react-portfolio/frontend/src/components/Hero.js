e47935b2a1e02e77f3f3af7f94fa6393c26d1e1a
pavan ch <pavan_fw10_057@masai.school>
2021-11-12 19:54:07 +0530

    changes made again

--- react-portfolio/frontend/src/components/Hero.js
+++ react-portfolio/frontend/src/components/Hero.js
@@ -24,7 +24,7 @@ const Hero = () => {
       </div>
       <div className='max-w-md mb-auto space-y-5'>
       <img style={{width:'230px'}} alt=''  src={pavan} />
-        <h1 className='text-5xl font-bold md:text-7xl'>Hello. I’m pavan</h1>
+        <h1 className='text-5xl font-bold md:text-7xl'>Hello. I’m Pavan</h1>
         <p className='tracking-wide leading-relaxed'>
           I have a passion for developing user-friendly, accessible and responsive websites. I never stop learning and for me, each new project is another adventure.{' '}
         </p>
